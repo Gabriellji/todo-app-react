efb2c7b06a39f7112d7f96370d5a340065204f0f
Gabriellji <gabriellji@yahoo.com>
2020-11-18 15:26:34 +0300

    style: components

--- src/components/SearchPanel.js
+++ src/components/SearchPanel.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import './SearchPanel.css';
+
 const SearchPanel = () => {
-    const searchText = 'Type here to search';
-    return <input
-        placeholder={searchText} />;
-}
+  return (
+    <input type="text"
+              className="form-control search-input"
+              placeholder="type to search" />
+  );
+};
 
 export default SearchPanel;
